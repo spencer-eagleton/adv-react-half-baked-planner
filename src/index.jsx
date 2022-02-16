7e90cb49b136d0d20dec1ff7c6e11ccad923cd43
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-02-16 11:56:35 -0800

    wrapped app in planner provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { PlannerProvider } from './context/PlannerContext';
 
 render(
   <React.StrictMode>
+    <PlannerProvider>
+
     <App />
+    </PlannerProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
